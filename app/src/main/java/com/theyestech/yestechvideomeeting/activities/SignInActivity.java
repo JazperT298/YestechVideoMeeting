b548a050b871d4a486db4e57b4883df2dca2fad2
jazper298 <minatosjutsu@gmail.com>
2020-08-19 13:03:04 +0800

    sign up UI design

--- app/src/main/java/com/theyestech/yestechvideomeeting/activities/SignInActivity.java
+++ app/src/main/java/com/theyestech/yestechvideomeeting/activities/SignInActivity.java
@@ -2,15 +2,34 @@ package com.theyestech.yestechvideomeeting.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.theyestech.yestechvideomeeting.R;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private View view;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        context = this;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializeUI();
+    }
+
+    private void initializeUI(){
+        findViewById(R.id.tv_SignUp).setOnClickListener(v -> {
+            Intent intent = new Intent(context, SignUpActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/theyestech/yestechvideomeeting/activities/SignUpActivity.java
+++ app/src/main/java/com/theyestech/yestechvideomeeting/activities/SignUpActivity.java
@@ -2,15 +2,29 @@ package com.theyestech.yestechvideomeeting.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 import com.theyestech.yestechvideomeeting.R;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private View view;
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        context = this;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializeUI();
+    }
+    private void initializeUI(){
+        findViewById(R.id.iv_Back).setOnClickListener(v -> onBackPressed());
+        findViewById(R.id.tv_SignIn).setOnClickListener(v -> onBackPressed());
     }
 }
