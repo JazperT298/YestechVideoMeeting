509b742091db57c815ce17cc9b9cbdf3759cb4ff
jazper298 <minatosjutsu@gmail.com>
2020-08-20 11:45:41 +0800

    user sending video invitation function

--- app/src/main/java/com/theyestech/yestechvideomeeting/activities/OutgoingInvitationActivity.java
+++ app/src/main/java/com/theyestech/yestechvideomeeting/activities/OutgoingInvitationActivity.java
@@ -1,5 +1,6 @@
 package com.theyestech.yestechvideomeeting.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -7,18 +8,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 import com.theyestech.yestechvideomeeting.R;
 import com.theyestech.yestechvideomeeting.models.Users;
+import com.theyestech.yestechvideomeeting.network.ApiClient;
+import com.theyestech.yestechvideomeeting.network.ApiService;
+import com.theyestech.yestechvideomeeting.utils.Constants;
+import com.theyestech.yestechvideomeeting.utils.PreferenceManager;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class OutgoingInvitationActivity extends AppCompatActivity {
     private View view;
     private Context context;
 
-    private ImageView iV_MeetingType,iv_StopInvitation;
+    private ImageView iV_MeetingType, iv_StopInvitation;
     private TextView tv_FirstChar, tv_Username, tv_Email;
     private String meetingType;
     private Users users;
+
+    private PreferenceManager preferenceManager;
+    private String inviterToken = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,30 +47,89 @@ public class OutgoingInvitationActivity extends AppCompatActivity {
         context = this;
         users = (Users) getIntent().getSerializableExtra("users");
         meetingType = getIntent().getStringExtra("type");
+        preferenceManager = new PreferenceManager(context);
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(task -> {
+            if(task.isSuccessful() && task.getResult() != null){
+                inviterToken = task.getResult().getToken();
+            }
+        });
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         initializeUI();
+        if(meetingType != null && users != null){
+            initiateMeeting(meetingType, users.token);
+        }
     }
 
-    private void initializeUI(){
+    private void initializeUI() {
         iV_MeetingType = findViewById(R.id.iV_MeetingType);
         tv_FirstChar = findViewById(R.id.tv_FirstChar);
         tv_Username = findViewById(R.id.tv_Username);
         tv_Email = findViewById(R.id.tv_Email);
         iv_StopInvitation = findViewById(R.id.iv_StopInvitation);
-        if(meetingType != null){
-            if(meetingType.equals("video")){
+        if (meetingType != null) {
+            if (meetingType.equals("video")) {
                 iV_MeetingType.setImageResource(R.drawable.ic_video);
             }
         }
-        if(users != null){
-            tv_FirstChar.setText(users.firstname.substring(0,1));
+        if (users != null) {
+            tv_FirstChar.setText(users.firstname.substring(0, 1));
             tv_Username.setText(String.format("%s %s", users.firstname, users.lastname));
             tv_Email.setText(users.email);
         }
         iv_StopInvitation.setOnClickListener(v -> onBackPressed());
+
+    }
+
+    private void initiateMeeting(String meetingType, String receiverToken){
+        try {
+            JSONArray tokens = new JSONArray();
+            tokens.put(receiverToken);
+
+            JSONObject body = new JSONObject();
+            JSONObject data = new JSONObject();
+
+            data.put(Constants.REMOTE_MSG_TYPE, Constants.REMOTE_MSG_INVITATION);
+            data.put(Constants.REMOTE_MSG_MEETING_TYPE, meetingType);
+            data.put(Constants.KEY_FIRST_NAME, preferenceManager.getString(Constants.KEY_FIRST_NAME));
+            data.put(Constants.KEY_LAST_NAME, preferenceManager.getString(Constants.KEY_LAST_NAME));
+            data.put(Constants.KEY_EMAIL, preferenceManager.getString(Constants.KEY_EMAIL));
+            data.put(Constants.REMOTE_MSG_INVITER_TOKEN, inviterToken);
+
+            body.put(Constants.REMOTE_MSG_DATA, data);
+            body.put(Constants.REMOTE_MSG_REGISTRATION_IDS, tokens);
+
+            sendRemoteMessage(body.toString(), Constants.REMOTE_MSG_INVITATION);
+        }catch (Exception e){
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    private void sendRemoteMessage(String remoteMessageBody, String type){
+        ApiClient.getClient().create(ApiService.class).sendRemoteMessage(Constants.getRemoteMessageHeader(), remoteMessageBody)
+                .enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(@NonNull Call<String> call,@NonNull  Response<String> response) {
+                        if(response.isSuccessful()){
+                            if(type.equals(Constants.REMOTE_MSG_INVITATION)){
+                                Toast.makeText(context, "Invitation sent successfully", Toast.LENGTH_LONG).show();
+                            }
+                        }else  {
+                            Toast.makeText(context, response.message(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull  Call<String> call,@NonNull  Throwable t) {
+                        Toast.makeText(context, t.getMessage(), Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
     }
 }
--- app/src/main/java/com/theyestech/yestechvideomeeting/utils/Constants.java
+++ app/src/main/java/com/theyestech/yestechvideomeeting/utils/Constants.java
@@ -1,5 +1,7 @@
 package com.theyestech.yestechvideomeeting.utils;
 
+import java.util.HashMap;
+
 public class Constants {
 
     public static final String KEY_COLLECTION_USERS = "users";
@@ -12,4 +14,21 @@ public class Constants {
 
     public static final String KEY_PREFERENCE_NAME = "videoMeetingPreference";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+
+    public static final String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+
+    public static final String REMOTE_MSG_TYPE = "type";
+    public static final String REMOTE_MSG_INVITATION = "invitation";
+    public static final String REMOTE_MSG_MEETING_TYPE = "meetingType";
+    public static final String REMOTE_MSG_INVITER_TOKEN = "inviterToken";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
+
+    public static HashMap<String, String> getRemoteMessageHeader(){
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put(Constants.REMOTE_MSG_AUTHORIZATION, "key=AAAAbIpzQjg:APA91bHQUeNSNLN3o6DhStDo8mL78FjoPPzRN6VgIeOf-fMAFez5rU8ntyQ7RpIHE0OQD4i6Ha-HPMoqslLHMR152UboG3qKt7R0tXtBSCln5CnhwEaUnzDDO_J-GQ0SX1Cr5izGBFl9");
+        headers.put(Constants.REMOTE_MSG_CONTENT_TYPE, "application/json");
+        return headers;
+    }
 }
