00359acea7e1222a6439bcc63c89ba3de5293687
jazper298 <minatosjutsu@gmail.com>
2020-08-20 12:16:37 +0800

    initiate video meeting || send meeting invitation

--- app/src/main/java/com/theyestech/yestechvideomeeting/activities/IncomingInvitationActivity.java
+++ app/src/main/java/com/theyestech/yestechvideomeeting/activities/IncomingInvitationActivity.java
@@ -2,15 +2,70 @@ package com.theyestech.yestechvideomeeting.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.theyestech.yestechvideomeeting.R;
+import com.theyestech.yestechvideomeeting.models.Users;
+import com.theyestech.yestechvideomeeting.utils.Constants;
+import com.theyestech.yestechvideomeeting.utils.PreferenceManager;
 
 public class IncomingInvitationActivity extends AppCompatActivity {
+    private View view;
+    private Context context;
+
+    private ImageView iV_MeetingType, iv_AcceptInvitation, iv_RejectInvitation;
+    private TextView tv_FirstChar, tv_Username, tv_Email;
+    private String meetingType;
+    private Users users;
+
+    private PreferenceManager preferenceManager;
+    private String inviterToken = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incoming_invitation);
+        context = this;
+        //users = (Users) getIntent().getSerializableExtra("users");
+        meetingType = getIntent().getStringExtra("type");
+        preferenceManager = new PreferenceManager(context);
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(task -> {
+            if(task.isSuccessful() && task.getResult() != null){
+                inviterToken = task.getResult().getToken();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initializeUI();
+    }
+
+    private void initializeUI(){
+        iV_MeetingType = findViewById(R.id.iV_MeetingType);
+        iv_AcceptInvitation = findViewById(R.id.iv_AcceptInvitation);
+        iv_RejectInvitation = findViewById(R.id.iv_RejectInvitation);
+        tv_FirstChar = findViewById(R.id.tv_FirstChar);
+        tv_Username = findViewById(R.id.tv_Username);
+        tv_Email = findViewById(R.id.tv_Email);
+        if (meetingType != null) {
+            if (meetingType.equals("video")) {
+                iV_MeetingType.setImageResource(R.drawable.ic_video);
+            }
+        }
+        String firstname = getIntent().getStringExtra(Constants.KEY_FIRST_NAME);
+        if(firstname != null){
+            tv_FirstChar.setText(firstname.substring(0,1));
+        }
+        tv_Username.setText(String.format("%s %s", firstname, getIntent().getStringExtra(Constants.KEY_LAST_NAME)));
+        tv_Email.setText(getIntent().getStringExtra(Constants.KEY_EMAIL));
+
     }
 }
--- app/src/main/java/com/theyestech/yestechvideomeeting/firebase/MessagingService.java
+++ app/src/main/java/com/theyestech/yestechvideomeeting/firebase/MessagingService.java
@@ -1,5 +1,6 @@
 package com.theyestech.yestechvideomeeting.firebase;
 
+import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -7,6 +8,8 @@ import androidx.annotation.NonNull;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.theyestech.yestechvideomeeting.activities.IncomingInvitationActivity;
+import com.theyestech.yestechvideomeeting.utils.Constants;
 
 public class MessagingService extends FirebaseMessagingService {
 
@@ -19,8 +22,20 @@ public class MessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        if(remoteMessage.getNotification() != null){
-            Log.d("FCM", "Remote message received " + remoteMessage.getNotification().getBody());
+//        if(remoteMessage.getNotification() != null){
+//            Log.d("FCM", "Remote message received " + remoteMessage.getNotification().getBody());
+//        }
+        String type = remoteMessage.getData().get(Constants.REMOTE_MSG_TYPE);
+        if(type != null){
+            if(type.equals(Constants.REMOTE_MSG_INVITATION)){
+                Intent intent = new Intent(getApplicationContext(), IncomingInvitationActivity.class);
+                intent.putExtra(Constants.REMOTE_MSG_MEETING_TYPE, remoteMessage.getData().get(Constants.REMOTE_MSG_MEETING_TYPE));
+                intent.putExtra(Constants.KEY_FIRST_NAME, remoteMessage.getData().get(Constants.KEY_FIRST_NAME));
+                intent.putExtra(Constants.KEY_LAST_NAME, remoteMessage.getData().get(Constants.KEY_LAST_NAME));
+                intent.putExtra(Constants.KEY_EMAIL, remoteMessage.getData().get(Constants.KEY_EMAIL));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
         }
     }
 }
